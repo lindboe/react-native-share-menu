366602a78a32ff8baf7a8179a6ebec65b8bf4b9f
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-01-30 14:57:18 -0800

    Rename things and set up share extension

--- examples/custom-rn-extension/android/app/src/main/java/com/customrnshareextensionexample/MainApplication.kt
+++ examples/custom-rn-extension/android/app/src/main/java/com/customrnshareextensionexample/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.customrnshareextensionexample
+package com.customrnshareExample
 
 import android.app.Application
 import com.facebook.react.PackageList
--- examples/custom-rn-extension/ios/customRNShareExampleTests/customRNShareExampleTests.m
+++ examples/custom-rn-extension/ios/customRNShareExampleTests/customRNShareExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface customRNShareExtensionExampleTests : XCTestCase
+@interface customRNShareExampleTests : XCTestCase
 
 @end
 
-@implementation customRNShareExtensionExampleTests
+@implementation customRNShareExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
